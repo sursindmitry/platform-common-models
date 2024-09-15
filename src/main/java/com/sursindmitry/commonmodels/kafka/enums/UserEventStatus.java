974795e736fe7940029d2c3b475a54c1fceb8226
Dmitry Sursin <40328886+sursindmitry@users.noreply.github.com>
2024-09-15 20:07:38 +0700

    PRJ-127: Исправил UserEvent (#13)

---
+++ src/main/java/com/sursindmitry/commonmodels/kafka/enums/UserEventStatus.java
@@ -0,0 +1,10 @@
+package com.sursindmitry.commonmodels.kafka.enums;
+
+/**
+ * Статусы {@link com.sursindmitry.commonmodels.kafka.UserEvent}.
+ */
+public enum UserEventStatus {
+    CREATED,
+    UPDATED,
+    DELETED
+}
